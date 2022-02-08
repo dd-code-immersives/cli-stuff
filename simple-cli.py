7585bd17b6be1a474e484d957bb2a581698af534
dd-code-immersives <domilamad89@gmail.com>
2022-02-08 18:43:38 +0100

    added default to number to avoid comparison error

--- simple-cli.py
+++ simple-cli.py
@@ -10,7 +10,7 @@ def random_cli():
 
 	# build CLI 
 	parser = argparse.ArgumentParser(description="Random Number Generator")
-	parser.add_argument("-n","--number",action='store', type=int, help="generates n random numbers")
+	parser.add_argument("-n","--number",action='store', type=int, help="generates n random numbers", default=0)
 	parser.add_argument("--nrange",action='store', help="specifies max number", nargs=2, type=int)
 	args = parser.parse_args()
 
