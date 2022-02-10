8ba52d9515d9e0323dbec51a606420cb8b9d4672
ITO-Taro <84404661+ITO-Taro@users.noreply.github.com>
2022-02-10 17:04:23 -0500

    added 'raise Exceptions to update function to avoid faulty updates and implemented a functino for browse

--- contact_book.py
+++ contact_book.py
@@ -19,7 +19,7 @@ class FileHandler():
         df = self.csv_to_df()
         if 'id' in args_lst:
             i_d = int(args_lst[args_lst.index('id')+1])
-            msg = f"{'*'*5} Showing result for ID {i_d}. All other key words were ignored {'*'*5}\n"
+            msg = f"\n{'_'*5} Showing result for ID {i_d}. All other key words were ignored {'_'*5}\n"
             res = print(msg,df.loc[i_d, :])
         else:
             for num in range(len(args_lst)):
@@ -35,6 +35,8 @@ class FileHandler():
     def update(self, args_lst):
         '''Filters the df by the user's id. Make changes to only one user at a time'''
         df = self.csv_to_df()
+        if [True for i in args_lst if not i in df.columns]:
+            raise Exception(f"{'*'*5} Passed arguments contain faulty column name(s) update cancelled {'*'*5}")
         '''Saves the 'id' value'''
         if 'id' in args_lst:
             i_d = int(args_lst[args_lst.index('id')+1])
@@ -45,14 +47,28 @@ class FileHandler():
                         if args_lst[num] != 'id':
                             df.loc[i_d, args_lst[num]] = args_lst[num+1]
                         else:
-                            print("Chages to ID Prohibitted")
+                            print(f"{'*'*5} Chages to ID Prohibitted {'*'*5}")
                 print(df.loc[i_d, :])
                 confirmation = input("Save changes? type yes or no: ")
                 if confirmation.lower() == 'yes':
                     df.to_csv(self.infile)
+                    print(f"\n{'_'*10} Update Complete {'_'*10}\n")
                 else:
-                    return print("Update Cancelled")
+                    return print(f"\n{'*'*10} Update Cancelled {'*'*10}\n")
             else:
-                raise Exception("ID data type != int")
+                raise Exception(f"\n{'*'*10} ID data type != int {'*'*10}\n")
         else:
-            raise Exception("Need ID Number to Make Changes")
+            raise Exception(f"\n{'*'*10} Need ID Number to Make Changes {'*'*10}\n")
+
+    def browse(self, nums):
+        df = self.csv_to_df()
+        cnt = len(nums)
+        if cnt < 2:
+            '''When one number digit is passed'''
+            print(df.loc[nums[0], :])
+        elif cnt == 2:
+            '''When two numbers are pasased'''
+            print(df.loc[nums[0]:nums[1], :])
+        elif cnt > 2:
+            '''When three numbers are pasased'''
+            print(df.loc[nums[0]:nums[1]:nums[2], :])
